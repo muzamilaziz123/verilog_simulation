28fc2250be39090ab307e61fefff062b60d121cd
muzamilaziz123 <pdsboys1091@gmail.com>
2024-08-26 18:25:38 +0500

    try again

--- run_ci.sh
+++ run_ci.sh
@@ -14,8 +14,9 @@ fi
 
 # Define paths for your Verilog project
 VERILOG_FILE="arithunit.v"
+TB_VERILOG_FILE"tb_arithunit.cpp"
 VERILOG_PATH="sources_1/new/$VERILOG_FILE"
-
+TB_VERILOG_PATH = "sim_1/new/$TB_VERILOG_FILE"
 # Check if the Verilog file exists
 if [ ! -f "$VERILOG_PATH" ]; then
     echo "Error: Verilog file $VERILOG_PATH not found."
@@ -29,6 +30,13 @@ verilator --lint-only "$VERILOG_PATH" 2>&1 | tee lint_output.log
 echo "Running yosys on $VERILOG_FILE..."
 yosys -p "read_verilog $VERILOG_PATH; synth -top arithunit; write_verilog -noattr synthesized_arithunit.v" 2>&1 | tee yosys_output.log
 
+verilator --cc "$VERILOG_FILE" --exe --build "$TB_VERILOG_FILE"2>&1 | tee verilator_compile.log
+./obj_dir/V"$TOP_MODULE" 2>&1 | tee verilator_simulation.log
+
+
+
+#verilator --cc "$VERILOG_FILE" --exe --build "sim_1/new/tb_$TOP_MODULE.cpp" 2>&1 | tee verilator_compile.log
+#./obj_dir/V"$TOP_MODULE" 2>&1 | tee verilator_simulation.log
 
 # Notify completion
 echo "Linting completed. Check lint_output.log for details."
