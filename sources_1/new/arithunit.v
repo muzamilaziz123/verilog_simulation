47ff31f3facccaa6bed252fe8dba760e72130fd2
muzamilaziz123 <108215424+muzamilaziz123@users.noreply.github.com>
2024-08-30 14:49:39 +0500

    Update arithunit.v

--- sources_1/new/arithunit.v
+++ sources_1/new/arithunit.v
@@ -1,31 +1,27 @@
 `timescale 1ns / 1ps
-module arithunit(
-input 				clk,
-input 				reset,
-input [15:0] 		data_1,
-input [15:0] 		data_2,
-input [1:0] 		op_sel,
-output reg [15:0] 	data_out 
 
+module arithunit(
+    input  logic             clk,
+    input  logic             reset,
+    input  logic [15:0]      data_1,
+    input  logic [15:0]      data_2,
+    input  logic [1:0]       op_sel,
+    output logic [15:0]      data_out
 );
 
-always@(*)
-	begin
-		if(reset)
-			begin		
-				data_out=0;
-			
-			end
-		else
-			begin
-				case (op_sel) 	
-					2'b00: data_out = data_1 + data_2;
-					2'b01: data_out = data_1 - data_2;
-					2'b10: data_out = data_1 * data_2;
-					2'b11: data_out = data_1 & data_2;
-					default: data_out = 16'b00;
-				endcase
-			end
-	end
+always_comb begin
+    if (reset) begin
+        data_out = 0;
+    end else begin
+        case (op_sel)
+            2'b00: data_out = data_1 + data_2;
+            2'b01: data_out = data_1 - data_2;
+            2'b10: data_out = data_1 * data_2;
+            2'b11: data_out = data_1 & data_2;
+            default: data_out = 16'b0;
+        endcase
+    end
+end
 
 endmodule
+
